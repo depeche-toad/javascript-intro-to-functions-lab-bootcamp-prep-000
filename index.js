2c0af7e84f80259ae056d9db2a4b0977fbfe62f8
Claire Kirby <ckirby921@gmail.com>
2017-09-14 15:52:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHitoGrandma(string)  {
-  return string("I can't hear you")
+if (`string` = 'lowercase') {
+  return string("I can't hear you") }
 }
